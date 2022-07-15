ea3af39dafd4b0ca8406939c157f240dad97dc0a
Shaswat Prabhat <spd1238@gmail.com>
2022-07-15 12:14:24 +0530

    Upgrade REact native

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,20 +6,32 @@
  * @flow strict-local
  */
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import type { Node } from "react";
 import AppWrapper from "../AppWrapper/AppWrapper";
-import { Button, Linking } from "react-native";
+import { Button, Linking, Text } from "react-native";
+import axios from "axios";
 
 const App: () => Node = () => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get("https://news.ycombinator.com/news");
+      setData(response);
+    };
+    fetchData();
+  }, []);
+
   return (
     <AppWrapper>
       <Button
+        color="red"
         title="Fetch News"
         onPress={async () => {
           await Linking.openURL("https://news.ycombinator.com/news");
         }}
       />
+      <Text>{JSON.stringify(data)}</Text>
     </AppWrapper>
   );
 };
